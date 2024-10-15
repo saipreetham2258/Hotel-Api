13c0d38efd31caa79d335868e82a5259f262b9c1
saipreetham <preetham.r@techouts.com>
2024-10-15 18:38:21 +0530

    hello

---
+++ src/main/kotlin/com/example/Database/DataBaseConnectivity.kt
@@ -0,0 +1,13 @@
+package com.example.Database
+
+import com.example.module.HotelDetails
+import com.mongodb.client.MongoCollection
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.getCollection
+
+object DataBaseConnectivity {
+        val string = "mongodb://localhost:27017"
+        val client = KMongo.createClient(string)
+        val dataBase = client.getDatabase("KotlinDataBase")
+        val collection  = dataBase.getCollection<HotelDetails>()
+}
